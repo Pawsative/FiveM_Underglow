887fad8eaff4d424812718190a2f317cfa1144f2
Pawsative <8021703+Pawsative@users.noreply.github.com>
2021-01-03 20:35:15 -0500

    Formatted code & added Config.DefaultKeyBind

--- core/shared/sh_config.lua
+++ core/shared/sh_config.lua
@@ -1,4 +1,5 @@
 Config = {}
 Config.UseKeybind = true
+Config.DefaultKeyBind = "g"
 Config.ChatCommand = "ToggleNeons" -- If UseKeybind is false, it'll use this chat command.
 Config.Delay = 1250 -- Time in MS, how often they can turn the neons on/off.
